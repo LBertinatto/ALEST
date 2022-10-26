f5d033c9098546e5696b505c411102feb3da7634
Leonardo Bertinatto <lforquim@hotmail.com>
2022-10-26 12:38:30 -0300

    Correção de Bugs

--- Funcionarios/Entregador.java
+++ Funcionarios/Entregador.java
@@ -1,46 +1,56 @@
 package Funcionarios;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import Models.Funcionario;
 import Models.TipoFuncionario;
 
 public class Entregador extends Funcionario{
-
+    
     private int espera;
     private int tolerancia;
-    private int contCancelados;
-    private int contQuaseCancelados;
-    private int contEntregues;
+    private int contEntregasSolicitadas = 0;
+    private int contCancelados = 0;
+    private int contQuaseCancelados = 0;
+    private int contEntregues = 0;
+    private boolean prontoParaEntrega;
     private ArrayList<Entregador> entregadores;
+    private static Queue<Pedido> filaDeEntrega;
 
     public Entregador(String nome, TipoFuncionario tipoFuncionario, int idFuncionario, boolean isEmpty,
             int qtdPedidos) {
         super(nome, tipoFuncionario, idFuncionario, isEmpty, qtdPedidos);
-
-        contCancelados = 0;
 
         Entregador entregador1 = new Entregador("entregador1", TipoFuncionario.ENTREGADOR, 1,true, 0);
         Entregador entregador2 = new Entregador("entregador1", TipoFuncionario.ENTREGADOR, 1, true, 0);
         Entregador entregador3 = new Entregador("entregador1", TipoFuncionario.ENTREGADOR, 1, true, 0);
 
         entregadores = new ArrayList<>();
+        filaDeEntrega = new LinkedList<>();
 
         entregadores.add(entregador1);
         entregadores.add(entregador2);
         entregadores.add(entregador3);
-
     }
     
     public void processamentoEntregador(){
             for (Pedido pedido : filaDeEntrega) { 
                 for (Funcionario funcionario : entregadores) {
-                    if(funcionario.isEmpty()){
+                    if(funcionario.isEmpty() && isProntoParaEntrega(pedido)){
                         System.out.println("Seu pedido está a caminho...");
+
                         pedido.setFuncionarioStatus(funcionario); // Atribui um pedido à um funcionário 
+
+                         funcionario.setEmpty(false); //Ocupa o funcionário
                         tempoDeEntrega(pedido);
+
                         //TODO: fazer sleep com relação ao tempo de entrega
+
                         cancelado(pedido);
+
                         if(!cancelado(pedido))funcionario.setQtdPedidos(funcionario.getQtdPedidos()+1);//Incrementa o contador de pedidos do funcionário
+                        
                         filaDeEntrega.poll(); // Retira a head da fila de espera
                         System.out.printf("Processando %s", pedido.getNome());
                         break;
@@ -59,18 +69,20 @@ public class Entregador extends Funcionario{
             contCancelados++;
             return true;
             }
-        }
+        
         if((tolerencia(p) - p.getTempoDeEspera()) <= 2){
             contQuaseCancelados++;
             contEntregues++;
             return false;
         }
+    
         contEntregues++; 
-        return false;
+    }
+            return false;
     }
 
     public boolean entregando(Pedido pedido){
-        if(entregadores.contains(pedido.getFuncionarioStatus())) return true;//Se o pedido já está com um entregador
+        if(entregadores.contains(pedido.getFuncionarioStatus()) && filaDeEntrega.contains(pedido)) return true;//Se o pedido já está com um entregador e o pedido ainda está na fila de entrega
         return false;
     }
 
@@ -83,5 +95,14 @@ public class Entregador extends Funcionario{
     public int tolerencia(Pedido pedido){
         tolerancia = pedido.getQtdProdutos() + 8;
         return tolerancia;
+    }
+
+    public boolean isProntoParaEntrega(Pedido p) {
+        if(Pedido.isSeparado(p)){
+            filaDeEntrega.add(p);
+            contEntregasSolicitadas++;
+            return true;
+         }
+         return false;
     }
 }
